1a7ebe2b6b8415102446a4a107a80fba155cab7a
ajaybabuchavan-cmd <ajaybabuchavan@eng.rizvi.edu.in>
2025-10-17 13:26:57 +0530

    Update E-1 TASK-1 INPUT.c

--- E-1 TASK-1 INPUT.c
+++ E-1 TASK-1 INPUT.c
@@ -1,6 +1,8 @@
+//Ajay Babu Chavan,UIN:-251P081,Year:-F.E,Division:-D
 #include <stdio.h>
 
 int main() {
     printf("Name:\t\tAjay Babu Chavan\nAge:\t\t17\nClass:\t\tFE (Computer Engineering)\nDivision:\tD\nUIN:\t\t251P081\n");
     return 0;
+
 }
