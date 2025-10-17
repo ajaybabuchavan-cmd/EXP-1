dc6ccdac99e8d26a08ded55af59aa8c2419c4f90
ajaybabuchavan-cmd <ajaybabuchavan@eng.rizvi.edu.in>
2025-10-17 13:35:50 +0530

    Add files via upload

---
+++ E-1 TASK-2 OUTPUT 2.c
@@ -0,0 +1,9 @@
+Enter marks in Physics: 18
+Enter marks in Chemistry: 18
+Enter marks in Mathematics: 18
+
+Average of PCM = 18.00
+Not eligible for admission.
+
+
+=== Code Execution Successful ===
